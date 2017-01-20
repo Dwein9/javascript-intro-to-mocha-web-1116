c9c8bc0d66eee191c024e71b394b4d7f9b4f99e3
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-20 15:17:40 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(f) {
+  return `I love ${f}`
+}
+
+function shouting(s) {
+  return s.toUpperCase()
+}
+
+function roundDown(n) {
+  return Math.floor(n)
+}
